41af1b0c7affa1406a9f529ccfebc5396c508787
Nataliia Hrytsyna <nataliia.hrytsyna@gmail.com>
2019-06-20 10:51:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,27 +11,27 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-  returns a / b
+  return a / b
+}
+
+function inc(n) {
+  return n++;
+}
+
+function inc(n) {
+  return n++;
+}
+
+function dec(n) {
+  return n--;
+}
+
+function makeInt(n) {
+  return parseInt(n)
 }
 
 
-
-
-
-it('inc(n) increments n and returns the result', function() {
-  expect(inc(a)).toEqual(a + 1)
-})
-
-it('dec(n) decrements n and returns the result', function() {
-  expect(dec(a)).toEqual(a - 1)
-})
-
-describe('makeInt(n)', function() {
-  it('parses n as an integer and returns the parsed integer', function() {
-    expect(makeInt(a.toString())).toEqual(a)
-  })
-
-  it('assumes base 10', function() {
+it('assumes base 10', function() {
     expect(makeInt('0x2328')).toEqual(0)
   })
 
