662bc6a4d2755d1eef8a3142849fa23a23d4a53b
Nataliia Hrytsyna <nataliia.hrytsyna@gmail.com>
2019-06-20 10:54:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -31,18 +31,13 @@ function makeInt(n) {
     return true;
   }
   else{ 
-  return parseInt(n, 10)
+  return parseInt(n, 10)}
 }
 
 function preserveDecimal(n) {
-  
+  if(isNaN(n)) {
+    return true;
+  }
+  return parseFloat(n);
 }
-describe('preserveDecimal(n)', function() {
-  it('preserves n\'s decimals (it parses n as a floating point number) and returns the parsed number', function() {
-    expect(preserveDecimal('2.222')).toBe(2.222)
-  })
 
-  it('returns NaN as appropriate', function() {
-    expect(isNaN(preserveDecimal('sldkjflksjf'))).toEqual(true)
-  })
-})
